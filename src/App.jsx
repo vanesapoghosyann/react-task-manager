6744e8ee3f6f6386004f63f762c03597b4990ae3
vanesapoghosyann <vanesapoghosyan2@gmail.com>
2026-07-13 21:18:45 +0400

    Implement task editing and clear completed tasks

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import TaskForm from "./components/TaskForm";
 
 function App() {
   const [tasks, setTasks] = useState(() => {
-  const savedTasks = localStorage.getItem("tasks");
+    const savedTasks = localStorage.getItem("tasks");
 
-  return savedTasks ? JSON.parse(savedTasks) : [];
-});
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
 
   const addTask = (taskText) => {
     const newTask = {
-  id: Date.now(),
-  text: taskText,
-  completed: false,
-};
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
 
     setTasks([...tasks, newTask]);
   };
@@ -25,6 +25,10 @@ function App() {
     setTasks(updatedTasks);
   };
 
+  const clearCompleted = () => {
+    const activeTasks = tasks.filter((task) => !task.completed);
+    setTasks(activeTasks);
+  };
 
   const toggleComplete = (index) => {
     const updatedTasks = tasks.map((task, i) =>
@@ -36,12 +40,34 @@ function App() {
     setTasks(updatedTasks);
   };
 
+  const startEditing = (task) => {
+    setEditingId(task.id);
+    setEditingText(task.text);
+  };
+
   const completedTasks = tasks.filter(task => task.completed).length;
   const remainingTasks = tasks.length - completedTasks;
 
-useEffect(() => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}, [tasks]);
+
+  const [editingId, setEditingId] = useState(null);
+  const [editingText, setEditingText] = useState("");
+
+
+  const saveTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id
+        ? { ...task, text: editingText }
+        : task
+    );
+
+    setTasks(updatedTasks);
+    setEditingId(null);
+    setEditingText("");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
 
   return (
@@ -49,30 +75,54 @@ useEffect(() => {
       <Header />
 
       <TaskForm onAddTask={addTask} />
-
+      <button onClick={clearCompleted}>
+        Clear Completed
+      </button>
       <p>Total Tasks: {tasks.length}</p>
       <p>Completed: {completedTasks}</p>
       <p>Remaining: {remainingTasks}</p>
 
+
+
       <ul>
         {tasks.map((task, index) => (
           <li key={task.id}>
-            <span
-              style={{
-                textDecoration: task.completed ? "line-through" : "none",
-                marginRight: "10px",
-              }}
-            >
-              {task.text}
-            </span>
+            {editingId === task.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editingText}
+                  onChange={(e) => setEditingText(e.target.value)}
+                />
 
-            <button onClick={() => toggleComplete(index)}>
-              {task.completed ? "Undo" : "Complete"}
-            </button>
+                <button onClick={() => saveTask(task.id)}>
+                  Save
+                </button>
+              </>
+            ) : (
+              <>
+                <span
+                  style={{
+                    textDecoration: task.completed ? "line-through" : "none",
+                    marginRight: "10px",
+                  }}
+                >
+                  {task.text}
+                </span>
 
-            <button onClick={() => deleteTask(index)}>
-              Delete
-            </button>
+                <button onClick={() => startEditing(task)}>
+                  Edit
+                </button>
+
+                <button onClick={() => toggleComplete(index)}>
+                  {task.completed ? "Undo" : "Complete"}
+                </button>
+
+                <button onClick={() => deleteTask(index)}>
+                  Delete
+                </button>
+              </>
+            )}
           </li>
         ))}
       </ul>
